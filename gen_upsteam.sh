cfefc8f51427d8ce8741a3dcb1220420f552a55e
Jerry <767729940@qq.com>
2021-02-16 11:02:46 +0800

    update

--- gen_upsteam.sh
+++ gen_upsteam.sh
@@ -4,3 +4,5 @@ wget https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/acc
 sed -i 's/114.114.114.114/\]tls:\/\/dot.pub/g' upstream_servers.list
 sed -i 's/server=/\[/g' upstream_servers.list
 sed -i '1i tls://dns.google' upstream_servers.list
+
+./AdGuardHome -s restart
