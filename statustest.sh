f8e6fc084ee898f6534c3933cd52994af1edf0ad
Jerry <767729940@qq.com>
2021-02-18 15:16:00 +0800

    状态监测改为多次监测的最好状态

--- statustest.sh
+++ statustest.sh
@@ -7,63 +7,66 @@
 #-o将curl下载的页面内容导出到/dev/null(默认会在屏幕显示页面内容)
 #-w设置curl命令需要显示的内容%{http_code}，指定curl返回服务器的状态码
 check(){
-       status_code=$(curl -s -o /dev/null -w %{http_code} --resolve www.notion.so:443:$1 https://www.notion.so )
-       if [ $status_code -ne 200 ];then
-              sleep 5
-              status_code=$(curl -s -o /dev/null -w %{http_code} --resolve www.notion.so:443:$1 https://www.notion.so )
-              if [ $status_code -ne 200 ];then
-                     status=4
-              else 
-                     status=1
-              fi
-       else
+       count=0
+       while [ $count -le 4 ];
+       do 
+       status_code=$(curl -s -o /dev/null -w %{http_code} --resolve www.notion.so:443:$1 https://www.notion.so)
+       #echo $status_code
+       if [ $status_code -eq 200 ];then
               status=1
+              break
+       else
+              count=`expr $count + 1`
+              echo $count
+              status=4
        fi
-#now=`date +%s`
-#echo $status
-#url=https://npstatus.jerryw.cn/api/v1/components/$2
-#echo $url
-curl  --url  https://npstatus.jerryw.cn/api/v1/components/$2 \
-  --request PUT \
-  -H "X-Cachet-Token: $Token"  \
-  -H 'Content-Type: application/json'  \
-  -d '{"status": '"$status"'}'
+       done
+
+       curl  --url  https://npstatus.jerryw.cn/api/v1/components/$2 \
+       --request PUT \
+       -H "X-Cachet-Token: $Token"  \
+       -H 'Content-Type: application/json'  \
+       -d '{"status": '"$status"'}'
 }
 
 s3check(){
+       count=0
+       while [ $count -le 4 ];
+       do 
        status_code=$(curl -s -o /dev/null -w %{http_code} --resolve s3.us-west-2.amazonaws.com:443:$1 \
-        https://s3.us-west-2.amazonaws.com )
-       if [ $status_code -ne 307 ];then
-              sleep 5
-              status_code=$(curl -s -o /dev/null -w %{http_code} --resolve s3.us-west-2.amazonaws.com:443:$1 \
-              https://s3.us-west-2.amazonaws.com )
-              if [ $status_code -ne 307 ];then
-                     status=4
-              else 
-                     status=1
-              fi
-       else
+        https://s3.us-west-2.amazonaws.com )       
+       if [ $status_code -eq 307 ];then
               status=1
+              break
+       else
+              count=`expr $count + 1`
+              echo $count
+              status=4
        fi
-#now=`date +%s`
-#echo $status
-#url=https://npstatus.jerryw.cn/api/v1/components/$2
-#echo $url
-curl  --url  https://npstatus.jerryw.cn/api/v1/components/$2 \
-  --request PUT \
-  -H "X-Cachet-Token: $Token"  \
-  -H 'Content-Type: application/json'  \
-  -d '{"status": '"$status"'}'
+       done
+
+       curl  --url  https://npstatus.jerryw.cn/api/v1/components/$2 \
+       --request PUT \
+       -H "X-Cachet-Token: $Token"  \
+       -H 'Content-Type: application/json'  \
+       -d '{"status": '"$status"'}'
 }
 
 
 dotcheck(){
-       msg=$(getdns_query @45.133.119.184~$1 -m -s -L -A www.baidu.com | grep www.baidu.com | wc -l)
-       if [ $msg -ne 0 ];then
+       count=0
+       while [ $count -le 4 ];
+       do 
+       msg=$(getdns_query @45.133.119.184~$1 -m -s -L -A www.baidu.com | grep answer_type | wc -l)
+       if [ $msg -ge 0 ];then
               status=1
-       else 
+              break
+       else
+              count=`expr $count + 1`
+              echo $count
               status=4
        fi
+       done
        curl  --url  https://npstatus.jerryw.cn/api/v1/components/$2 \
        --request PUT \
        -H "X-Cachet-Token: $Token"  \
@@ -73,14 +76,20 @@ dotcheck(){
 
 dohcheck()
 {
+       count=0
+       while [ $count -le 4 ];
+       do 
        status_code=$(curl -H 'accept: application/dns-message' \
-       "$1?dns=UyQBAAABAAAAAAAAA3d3dwViYWlkdQNjb20AAAEAAQ" \
-         -w %{http_code} -s -o /dev/null)
-       if [ $status_code -ne 200 ];then
-              status=4
-       else 
+         "$1?dns=UyQBAAABAAAAAAAAA3d3dwViYWlkdQNjb20AAAEAAQ"  -w %{http_code} -s -o /dev/null)       
+       if [ $status_code -eq 200 ];then
               status=1
+              break
+       else
+              count=`expr $count + 1`
+              echo $count
+              status=4
        fi
+       done
        curl  --url  https://npstatus.jerryw.cn/api/v1/components/$2 \
        --request PUT \
        -H "X-Cachet-Token: $Token"  \
@@ -90,6 +99,8 @@ dohcheck()
 
 Token=
 check 119.28.13.121 1
+check 194.56.79.104 10
 s3check 119.28.13.121 7
+s3check 194.56.79.104 11
 dotcheck dns.jerryw.cn 8
 dohcheck https://dns.jerryw.cn:8443/dns-query 9
